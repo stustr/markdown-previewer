a76dc0d922330203c4be4a6289c8c24347757321
1919dev <sfbsturrock@gmail.com>
2022-10-05 20:48:19 +0100

    app update incl. sass styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.scss";
 
 function App() {
+  const [text, setText] = useState(`
+  # This is a markdown previewer
+  You can use headings like these:
+  # H1
+  ## H2
+  Make a title like this:
+  [title](https://www.example.com)
+  
+  Insert code inline: \`<div></div\`,
+  
+  or in a block:
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Sarah",
+    "age": 25
+  }
+  \`\`\`
+  
+  Feature a list:
+  - First item
+    - Second item
+      - Third item
+  
+  Add a ...
+  > blockquote
+  
+  Transform things to **bold** or *italicised*
+  
+  and even add an image:
+  ![markdownlogo](https://upload.wikimedia.org/wikipedia/commons/3/37/Markdown-mark-solid.svg)
+  `);
+
+  marked.setOptions({
+    breaks: true,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        value={text}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(text) }}
+      ></div>
     </div>
   );
 }
